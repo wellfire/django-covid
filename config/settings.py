898881313323b56097db743b6b9d4d0086c5ae15
Alex Little <alex@digital-campus.org>
2020-04-09 12:15:52 +0300

    update logo

--- config/settings.py
+++ config/settings.py
@@ -124,10 +124,10 @@ MODELTRANSLATION_DEFAULT_LANGUAGE = 'en'
 #####################################################################
 # Static assets & media uploads
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'orb/static')
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
 #####################################################################
 
 
