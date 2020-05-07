3260fdd3dbf930a1ca6413c1251fe9a2ecefc59e
Ben Lopatin <ben@benlopatin.com>
2020-05-07 18:14:08 -0400

    Add GitHub Action workflow for testing
    - Include matrix versions but do not fail fast
    - Include base settings in ci_settings

--- config/ci_settings.py
+++ config/ci_settings.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-#import ctypes
 import os
 
 import dj_database_url  # NOTE: This is not in the project requirements!
 
+from .settings import *
 
-DATABASES = {'default': dj_database_url.config(default='mysql://root@localhost/orb')}
+
+DATABASES = {'default': dj_database_url.config(default='mysql://root:root@127.0.0.1:3306/orb')}
 DATABASES['default']['TEST'] = {'NAME': os.environ.get('TEST_DB_NAME', 'test_orb')}
 
 HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.BaseSignalProcessor'
