87a24450e874053aa5b34b6c5b549ef0f7367676
Ben Lopatin <ben@benlopatin.com>
2020-05-08 14:40:42 -0400

    Use SQLite for testing for now

--- config/ci_settings.py
+++ config/ci_settings.py
@@ -6,11 +6,14 @@ import dj_database_url  # NOTE: This is not in the project requirements!
 from .settings import *
 
 
-DATABASES = {'default': dj_database_url.config(default='mysql://root:root@127.0.0.1:3306/orb')}
-DATABASES['default']['TEST'] = {'NAME': os.environ.get('TEST_DB_NAME', 'test_orb')}
+#DATABASES = {
+#    "default": dj_database_url.config(default="mysql://root:root@127.0.0.1:3306/orb")
+#}
+#DATABASES["default"]["TEST"] = {"NAME": os.environ.get("TEST_DB_NAME", "test_orb")}
 
-HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.BaseSignalProcessor'
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "orb",}}
 
-#GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH', '/usr/local/lib/libgdal.dylib')
-#ctypes.CDLL(GDAL_LIBRARY_PATH)
+HAYSTACK_SIGNAL_PROCESSOR = "haystack.signals.BaseSignalProcessor"
 
+# GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH', '/usr/local/lib/libgdal.dylib')
+# ctypes.CDLL(GDAL_LIBRARY_PATH)
