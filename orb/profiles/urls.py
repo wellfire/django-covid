5e0d43356796bafd75493f33dd377ab4e25bb7bd
Ben Lopatin <ben@benlopatin.com>
2020-05-20 15:15:48 -0400

    Replace django.contrib.auth.views.logout with LogoutView

--- orb/profiles/urls.py
+++ orb/profiles/urls.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import url
-from django.contrib.auth.views import logout
+from django.contrib.auth.views import LogoutView
 from django.views.generic import TemplateView
 from django.views.i18n import set_language
 
@@ -11,7 +11,7 @@ urlpatterns = [
     url(r'^register/$', view=views.RegistrationView.as_view(), name="profile_register"),
     url(r'^register/thanks/$', TemplateView.as_view(template_name="orb/thanks.html"), name="profile_register_thanks"),
     url(r'^login/$', views.login_view, name="profile_login"),
-    url(r'^logout/$', logout, {'template_name': 'orb/logout.html'}, name="profile_logout"),
+    url(r'^logout/$', LogoutView.as_view(template_name='orb/logout.html'), name="profile_logout"),
     url(r'^setlang/$', set_language, name="profile_set_language"),
     url(r'^reset/$', views.reset, name="profile_reset"),
     url(r'^reset/sent/$', TemplateView.as_view(template_name="orb/profile/reset-sent.html"), name="profile_reset_sent"),
