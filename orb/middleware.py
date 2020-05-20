e2a9e5ae9e68d477e3c4ba49df76f28806a363ee
Ben Lopatin <ben@benlopatin.com>
2020-05-20 14:25:37 -0400

    Update MIDDLEWARE setting and custom middleware class

--- orb/middleware.py
+++ orb/middleware.py
@@ -1,15 +1,17 @@
-
 from __future__ import unicode_literals
 
 from orb.forms import HeaderSearchForm
 
 
 class SearchFormMiddleware(object):
+    """Adds HeaderSearchForm to request as `search_form`"""
 
-    def process_request(self, request):
+    def __init__(self, get_response):
+        self.get_response = get_response
 
+    def __call__(self, request):
         form = HeaderSearchForm()
-
         # attach the form to the request so it can be accessed within the
         # templates
         request.search_form = form
+        return self.get_response(request)
