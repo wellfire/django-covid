fd8c84ff30b7b28745b706e6332a66cd49def166
Ben Lopatin <ben@benlopatin.com>
2020-05-20 14:25:37 -0400

    Update MIDDLEWARE setting and custom middleware class

--- config/settings.py
+++ config/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
 ]
 
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.locale.LocaleMiddleware',
